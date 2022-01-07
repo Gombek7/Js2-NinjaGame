c44ff5c23b48427326e790bbbfb348c679b4601b
Gombek7 <gombek7@gmail.com>
2022-01-07 12:43:48 +0100

    Styled GAME OVER text

--- src/scripts/objects/gameOverText.ts
+++ src/scripts/objects/gameOverText.ts
@@ -5,5 +5,6 @@ export default class GameOverText extends Phaser.GameObjects.Text {
     this.setOrigin(0)
     this.setScrollFactor(0)
     this.setText('GAME OVER')
+    this.setStyle({ "color": "#a10000ff", "fontSize": "120px", "fontStyle": "bold", "stroke": "#000000ff", "strokeThickness":2,"shadow.offsetX":1,"shadow.offsetY":-1,"shadow.blur":1,"shadow.stroke":true});
   }
 }
